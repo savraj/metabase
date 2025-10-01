be219235df86a1b10679bd5927c0a78e5c61944d
Ryan Laurie <iethree@gmail.com>
2025-10-01 17:44:22 -0600

    make routing less bad

--- frontend/src/metabase/lib/urls/transforms.ts
+++ frontend/src/metabase/lib/urls/transforms.ts
@@ -1,17 +1,20 @@
+import { PLUGIN_TRANSFORMS } from "metabase/plugins";
 import type { Transform } from "metabase-types/api";
 
+const ROOT_URL = PLUGIN_TRANSFORMS.ROOT_URL;
+
 export function newNativeTransform() {
-  return `/admin/transforms/new/native`;
+  return `${ROOT_URL}/new/native`;
 }
 
 export function newPythonTransform() {
-  return `/admin/transforms/new/python`;
+  return `${ROOT_URL}/new/python`;
 }
 
 export function transform(id: Transform["id"]) {
-  return `/admin/transforms/${id}`;
+  return `${ROOT_URL}/${id}`;
 }
 
 export function transformEdit(id: Transform["id"]) {
-  return `/admin/transforms/${id}/query`;
+  return `${ROOT_URL}/${id}/query`;
 }
